b3c9c0058ab635c563ba2655612cbbd4f82f851e
Abbas-khaliq <abbas.khaliq@gmail.com>
2022-02-10 16:01:22 +0530

    fix(composer.json): installation error [skip-ci]

--- src/Utils/ImpressionBuilder.php
+++ src/Utils/ImpressionBuilder.php
@@ -30,7 +30,7 @@ class ImpressionBuilder
     /**
      * sdk version for api hit
      */
-    const SDK_VERSION = '1.32.2';
+    const SDK_VERSION = '1.32.3';
     /**
      * sdk langauge for api hit
      */
